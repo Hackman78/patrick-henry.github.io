757293e1b6abd43388549bec587fe54ca6b6c98f
Patrick Henry <Swagman78@icloud.com>
2023-08-16 03:09:18 +0000

    Finished fizz-buzz.js

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -10,13 +10,32 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
 */
+//function called fizzbuzz
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
-
+    var i = 1;
+while (i <= 100){
+  
+if ((i % 5) === 0 && (i % 3) === 0){
+   console.log('FizzBuzz')
+  i++
+}else if((i % 5) === 0){
+    console.log('Buzz')
+  i++
+}else if((i % 3) === 0){
+   console.log('Fizz')
+  i++
+}else{
+    console.log(i)
+  i++
+}
+   
+}
+    
 
     // YOUR CODE GOES ABOVE HERE //
 }
+
 
 
 
